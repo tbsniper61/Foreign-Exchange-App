071f180e775952f5c92312150e8167670508735a
tbsniper61 <tbsniper61@gmail.com>
2017-10-23 22:02:20 +0100

    Removed login and register links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Market rates</div>
+                <div class="panel-heading">Market rates as at {{ date('d/m/Y')}}</div>
 
                 <div class="panel-body">
                      <div class="currency-table">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,10 +48,8 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
+                       
+                       @if (auth()->user() and auth()->user()->is_admin )
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
